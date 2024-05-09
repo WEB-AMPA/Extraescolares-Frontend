4441bceb6a299289f890d235e7b5c08af5365ab1
AlejoxVargas <henrryalejandro21@gmail.com>
2024-05-09 16:12:44 +0200

    Fixed Tailwind Frontend configuration

--- src/app/pages/home/home.jsx
+++ src/app/pages/home/home.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import './home.css'
 
 function Home() {
   return (
-    <div>
-     <h1>Home</h1>
+    <div className="container mx-auto">
+     <h1 className="text-6xl">Esta es la primera parte de la landing</h1>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores distinctio corporis eos fugiat perspiciatis. Aliquid totam, fugit quasi fugiat minima, libero commodi adipisci vero hic provident deleniti expedita qui. Tempore.</p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,35 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
-      padding:{
-        default: '15px'
+      padding: {
+        default: "15px",
       },
     },
-    screens:{
-      sm: '640px',
-      md: '768px',
-      lg: '960px',
-      xl: '1200px',
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1200px",
     },
     fontFamily: {
-      primary: 'DM Serif Display',
-      secondary: 'Jost',
+      primary: "Poppins",
+      secondary: "Lexend",
     },
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          default: "#0D0D0D",
+          hover: "#F2E205"
+        },
+        secondary: "#736B62",
+        accent: {
+          default: "#EAF205",
+          secondary: "#BF9D7E",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
