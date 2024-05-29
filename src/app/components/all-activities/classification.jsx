8f6ca7cb2bc9a9293e0cbef4df8b305ee0143338
Erwin Massa <ermapla@gmail.com>
2024-05-29 14:31:41 +0200

    Feat styles for the first three sections

--- src/app/components/all-activities/classification.jsx
+++ src/app/components/all-activities/classification.jsx
@@ -4,8 +4,8 @@ const Classification = () => {
   return (
     <div className="px-6 pt-12 pb-20 bg-violet-500 bg-opacity-30 max-md:px-5">
       <div className="flex gap-5 max-md:flex-col max-md:gap-0 ">
-        <div className="flex flex-col w-[35%] max-md:ml-0 max-md:w-full">
-          <div className="text-base leading-6 text-black">
+        <div className="flex flex-col w-full max-md:ml-0 max-md:w-full">
+          <div className="ml-10 text-base leading-6 text-black">
             <span className="text-xl font-bold">Actividades Deportivas</span>
             <br />
             <ul>
@@ -22,11 +22,9 @@ const Classification = () => {
             <br />
           </div>
         </div>
-        <div className="flex flex-col ml-5 w-[26%] max-md:ml-0 max-md:w-full">
-          <div className="text-base leading-6 text-black">
-            <br />
+        <div className="flex gap-5 w-full max-md:ml-0 max-md:w-full">
+          <div className="ml-10 text-base leading-6 text-black">
             <span className="text-xl font-bold">Actividades Educativas</span>
-            <br />
             <ul>
               <li>Club de deberes.</li>
               <li>Creatividad y diversión.</li>
@@ -38,10 +36,8 @@ const Classification = () => {
             <br />
           </div>
         </div>
-        <div className="flex flex-col ml-5 w-[39%] max-md:ml-0 max-md:w-full">
+        <div className="flex flex-col  w-full max-md:ml-0 max-md:w-full">
           <div className="text-base leading-6 text-black">
-            <br />
-            <br />
             <span className="text-xl font-bold">
               Actividades para Niños/as con Dificultades de Aprendizaje
             </span>
--- src/app/components/all-activities/specificactivities.jsx
+++ src/app/components/all-activities/specificactivities.jsx
@@ -3,15 +3,15 @@
 const Specificactivities = () => {
   return (
     <div>
-      <div className="flex gap-5 max-md:flex-col max-md:gap-0">
+      <div className="mt-10 flex gap-5 max-md:flex-col max-md:gap-0">
         <div className="flex flex-col w-[46%] max-md:ml-0 max-md:w-full">
           <div className="flex flex-col px-5 mt-3.5 text-base leading-6 text-black max-md:mt-10">
-            <div>
-              <span className="text-xl font-bold">
+            <div className="text-center">
+              <span className=" text-xl font-bold">
                 Actividades para Papás y Mamás
               </span>
               <br />
-              <ul>
+              <ul className="list-disc list-inside">
                 <li>Cursos: Gimnasia, zumba, pilates, baile.</li>
                 <li>
                   Talleres: Temas relevantes como resolución de conflictos y
@@ -21,19 +21,19 @@ const Specificactivities = () => {
             </div>
             <img
               loading="lazy"
-              srcSet="..."
-              className="mt-20 w-full aspect-[1.49] max-md:mx-2.5 max-md:mt-10"
+              srcSet="/Pilates.png"
+              className="self-center mt-20 w-[250px] h-[150px] aspect-[1.45] max-md:mx-2.5 max-md:mt-10"
             />
           </div>
         </div>
-        <div className="flex flex-col ml-5 w-[54%] max-md:ml-0 max-md:w-full">
+        <div className="mt-2 flex flex-col ml-5 w-[54%] max-md:ml-0 max-md:w-full">
           <div className="flex flex-col grow px-5 text-base leading-6 text-black max-md:mt-10 max-md:max-w-full">
-            <div className="max-md:max-w-full">
+            <div className="text-center max-md:max-w-full">
               <span className="text-xl font-bold">
                 Eventos y Actividades Puntuales
               </span>
               <br />
-              <ul>
+              <ul className="list-disc list-inside">
                 <li>Cabalgata de Reyes Magos del distrito</li>
                 <li>
                   Fiestas y Eventos: Celebraciones de colegio, Navidad,
@@ -50,8 +50,8 @@ const Specificactivities = () => {
             </div>
             <img
               loading="lazy"
-              srcSet="..."
-              className="self-center mt-12 max-w-full aspect-[1.45] w-[350px] max-md:mt-10"
+              srcSet="/actividadespuntuales.png"
+              className="self-center mt-12 mb-10 w-[250px] h-[150px] aspect-[1.45] max-md:mx-2.5 max-md:mt-10"
             />
           </div>
         </div>
