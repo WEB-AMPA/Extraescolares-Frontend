94f5edc19802e2415fff75b13ee463a12b0647e4
RameshNada <ramesh.nadarajan@gmail.com>
2024-05-30 14:35:37 +0200

    frontend reset coding updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Layout from "../src/app/pages/layout/layout";
 import IntranetLayout from "./app/pages/layout/IntranetLayout";
 import Navbar from "./app/components/navbar/Navbar";
 import UsersList from "./app/pages/Usuarios/usuarios";
-import LoginPage from "./app/components/login/login";
+import nagigateToOtp from "./app/components/login/login.jsx";
+
 import ContactFrom from "./app/components/login/ContactForm.jsx";
 import ForgotPassword from "./app/components/login/forgotPassword.jsx";
 
@@ -16,7 +17,7 @@ function App() {
         <Route path="/" element={<Layout />} />
         <Route path="/intranet/*" element={<IntranetLayout />} />
         <Route path="/users" element={<UsersList />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<navigateToOtp />} />
         <Route path="/contactForm" element={<ContactFrom />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
       </Routes>
