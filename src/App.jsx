ba20d7e81bfdb40fa91f9530be5c75194ed2dc7a
soyshan <shanbu131@gmail.com>
2024-06-03 10:18:02 +0200

    feat: create attendances on a new branch

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import LayoutCentros from "./app/pages/layout/layoutCentros";
 import AboutUs from "./app/pages/layout/layout-about-us";
 import PrivacyPolicy from "./app/pages/privacyPolicy/privacyPolicy"
 
+
+import ActivitiesAttendance from "./app/pages/activitiesAttendance/activitiesAttendance";
+
+
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />} />
         <Route path="/centros" element={<LayoutCentros />} />
+        <Route path="/attendances" element={<ActivitiesAttendance />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/Privacy-Policy" element={<PrivacyPolicy />} />
       </Routes>
