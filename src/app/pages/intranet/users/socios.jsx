5b631e876b85236e69f6f1b6a681beeaaf88d46f
Yeshiprinss <martinezyessenia068@gmail.com>
2024-06-10 12:37:39 +0200

    feat:(searchbar):add searchbar

--- src/app/pages/intranet/users/socios.jsx
+++ src/app/pages/intranet/users/socios.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SociosTable from '../../../components/admin/users/sociosTable'
 import CrearSocio from "../../../components/admin/users/crearSocio";
+import SearchBar from "../../../components/admin/users/searchBar";
 
 
 function Socios () {
@@ -8,7 +9,10 @@ function Socios () {
 <>
 <div>
 <h1 className="flex justify-start text-3xl p-5 m-4 font-bold">Gestión de Socios</h1>
-<CrearSocio />
+<div className="flex justify-center items-center">
+      <CrearSocio />
+      <SearchBar />
+    </div>
 <SociosTable />
 </div>
 </>
