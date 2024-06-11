dc075b817422a0e0e3a8c5c8563e0bb3b74d62ac
RameshNada <ramesh.nadarajan@gmail.com>
2024-06-11 10:49:56 +0200

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./app/pages/layout/layout-landing.jsx";
+import Layout from "./app/pages/layout/layout-landing";
 import LayoutCentros from "./app/pages/layout/layoutCentros";
-import IntranetLayout from "./app/pages/layout/IntranetLayout";
-import UsersList from "./app/pages/Usuarios/usuarios";
-import ContactFrom from "./app/components/login/ContactForm.jsx";
-import Login from "./app/components/login/login.jsx";
-import OTPInput from "./app/components/login/OTPInput.jsx";
-import Recovered from "./app/components/login/Recovered.jsx";
-import Reset from "./app/components/login/Reset.jsx";
 import AboutUs from "./app/pages/layout/layout-about-us";
-import PrivacyPolicy from "./app/pages/privacyPolicy/privacyPolicy"
+import PrivacyPolicy from "./app/pages/privacyPolicy/privacyPolicy";
 import Contact from "./app/pages/contact/contact";
-import Login from "./app/components/login/login";
+import Login from "./app/components/login/login.jsx";
 import IntranetLayout from "./app/pages/layout/IntranetLayout";
 import OTPInput from "./app/components/login/OTPInput.jsx";
 import Recovered from "./app/components/login/Recovered.jsx";
@@ -49,14 +42,13 @@ function App() {
             <Route path="/Privacy-Policy" element={<PrivacyPolicy />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="/login" element={<NavigateComponents />} />
-            <Route path="/intranet" element={<IntranetLayout />} >
+            <Route path="/intranet" element={<IntranetLayout />}>
               {/* <Route path="/intranet" element={<IntranetLayout />} /> */}
               <Route path="students" element={<Students />} />
               <Route path="users" element={<UsersList />} />
             </Route>
           </Routes>
         </BrowserRouter>
-
       </RecoveryContext.Provider>
     </AuthContextProvider>
   );
