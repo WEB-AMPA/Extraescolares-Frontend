1fe64293d3e0a2e0f39ecc146cb840c946ef29a0
RameshNada <ramesh.nadarajan@gmail.com>
2024-06-11 10:13:49 +0200

    fix: post change

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { createContext, useState } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./app/pages/layout/layout";
+import Layout from "./app/pages/layout/layout-landing.jsx";
 import LayoutCentros from "./app/pages/layout/layoutCentros";
 import IntranetLayout from "./app/pages/layout/IntranetLayout";
 import UsersList from "./app/pages/Usuarios/usuarios";
 import ContactFrom from "./app/components/login/ContactForm.jsx";
-import Login from "./app/components/login/Login.jsx";
+import Login from "./app/components/login/login.jsx";
 import OTPInput from "./app/components/login/OTPInput.jsx";
 import Recovered from "./app/components/login/Recovered.jsx";
 import Reset from "./app/components/login/Reset.jsx";
 import AboutUs from "./app/pages/layout/layout-about-us";
-import PrivacyPolicy from "./app/pages/privacyPolicy/privacyPolicy"
+import PrivacyPolicy from "./app/pages/privacyPolicy/privacyPolicy";
 
 export const RecoveryContext = createContext();
 
