f863fe06e318e1a2b78013a3dc897ae4e76465cd
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-06-13 11:00:32 +0200

    feat: updated changes on login form

--- src/components/Web/login/LoginForm.jsx
+++ src/components/Web/login/LoginForm.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAuthContext } from '../../../context/authContext.jsx'; // Ajusta la ruta según tu estructura de proyecto
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../../context/authContext.jsx";
+import { useForm } from "../../../hooks/useForm.jsx";
 
 const LoginForm = () => {
-  const [usernameOrEmail, setUsernameOrEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const { usernameOrEmail, password, onInputChange, onResetForm } = useForm({
+    usernameOrEmail: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
   const { setAuth } = useAuthContext();
   const navigate = useNavigate();
 
@@ -13,10 +16,10 @@ const LoginForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/api/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ usernameOrEmail, password })
+      const response = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ usernameOrEmail, password }),
       });
 
       const data = await response.json();
@@ -26,9 +29,9 @@ const LoginForm = () => {
           const { role } = data;
 
           // Almacenar información del usuario en sessionStorage
-          sessionStorage.setItem('usernameOrEmail', usernameOrEmail);
-          sessionStorage.setItem('role', role);
-          sessionStorage.setItem('token', data.token);
+          sessionStorage.setItem("usernameOrEmail", usernameOrEmail);
+          sessionStorage.setItem("role", role);
+          sessionStorage.setItem("token", data.token);
 
           setAuth({
             isAuth: true,
@@ -38,17 +41,18 @@ const LoginForm = () => {
           });
 
           // Redirigir a la página de intranet
-          navigate('/intranet');
+          navigate("/intranet");
         } else {
-          setError('Credenciales incorrectas. Por favor, verifica tus datos.');
+          setError("Credenciales incorrectas. Por favor, verifica tus datos.");
         }
       } else {
-        setError('Error al iniciar sesión. Por favor, inténtalo de nuevo.');
+        setError("Error al iniciar sesión. Por favor, inténtalo de nuevo.");
       }
     } catch (error) {
-      console.error('Error al verificar las credenciales', error);
-      setError('Error al verificar las credenciales');
+      console.error("Error al verificar las credenciales", error);
+      setError("Error al verificar las credenciales");
     }
+    onResetForm();
   };
 
   return (
@@ -60,24 +64,33 @@ const LoginForm = () => {
           <div className="mb-4">
             <label className="block text-gray-700">Usuario o Email</label>
             <input
+              name="usernameOrEmail"
+              id="usernameOrEmail"
               type="text"
               className="w-full px-3 py-2 border rounded"
               value={usernameOrEmail}
-              onChange={(e) => setUsernameOrEmail(e.target.value)}
+              onChange={onInputChange}
               required
             />
           </div>
           <div className="mb-4">
             <label className="block text-gray-700">Contraseña</label>
             <input
+              name="password"
+              id="password"
               type="password"
               className="w-full px-3 py-2 border rounded"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={onInputChange}
               required
             />
           </div>
-          <button type="submit" className="w-full bg-blue-500 text-white py-2 rounded">Ingresar</button>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 rounded"
+          >
+            Ingresar
+          </button>
         </form>
       </div>
     </div>
